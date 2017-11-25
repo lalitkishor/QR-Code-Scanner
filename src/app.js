4bbc2e99449628feb5b087c1f3d041bc6998863d
LalitKishor <lalitsinghbanna05@gmail.com>
2017-11-25 23:37:48 +0530

    imporove ui and support back camera

--- src/app.js
+++ src/app.js
@@ -10,8 +10,15 @@ app.controller("barcodeCtrl",function($scope){
     });
 	Instascan.Camera.getCameras().then(function (cameras) {
 		if (cameras.length > 0) {
-		  var a = scanner.start(cameras[0]);
-		  console.log(a);
+		var selectedCam = scanner.start(cameras[0]);
+		 $.each(cameras, (i, c) => {
+         if (c.name.indexOf('back') != -1) {
+            	selectedCam = c;
+            return false;
+        	}
+        
+    	});
+		scanner.start(selectedCam);  
 		} else {
 		  console.error('No cameras found.');
 		}
